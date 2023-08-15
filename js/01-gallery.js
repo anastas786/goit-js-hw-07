60d18edb2a111d79ad202abda63df31e3c396835
anastas786 <agorbatuk08@gmail.com>
2023-08-15 19:16:00 +0200

    first task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,6 +3,45 @@ import { galleryItems } from './gallery-items.js';
 
 const container = document.querySelector('.gallery');
 
+function createMarkup(arr) {
 
-console.log(container);
-// console.log(galleryItems);
+  return arr
+  .map(({ preview, original, description }) => `
+    <li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      width = "200"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>` 
+)
+.join('');
+
+}
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+container.addEventListener('click', handlerImageClick);
+
+
+function handlerImageClick(evt) {
+  evt.preventDefault();
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+
+  const largeImage = evt.target.closest('.gallery__image');
+
+  const sourceUrl = largeImage.dataset.source;
+
+  const instance = basicLightbox.create(`
+<img src="${sourceUrl}" alt="${largeImage.src}" />
+`);
+
+  instance.show();
+
+}
+
